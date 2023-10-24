da7059d17c830f6f63fdbcda8b755d3287bbad21
haram <harami0007@naver.com>
2023-10-24 17:20:34 +0900

    같은 네비게이션 내에서 이동 성공

--- app/src/main/java/com/inhatc/haramnavigation/MainFragment.kt
+++ app/src/main/java/com/inhatc/haramnavigation/MainFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.inhatc.haramnavigation.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
@@ -26,10 +28,14 @@ class MainFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        super.onViewCreated(view, savedInstanceState);
 
         binding.apply {
-
+            // 버튼 클릭하면 페이지 이동
+            binding.btnMove.setOnClickListener {
+                val navController = findNavController()
+                navController.navigate(R.id.action_mainFragment_to_secondFragment)
+            }
         }
     }
 }
