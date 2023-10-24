34014bc5291c3d0e2b470ccde923eba5871c5209
haram <harami0007@naver.com>
2023-10-24 16:55:53 +0900

    하위메뉴 ãì구현 완료ㅠㅠㅠㅠㅠ

--- app/src/main/java/com/inhatc/haramnavigation/MainActivity.kt
+++ app/src/main/java/com/inhatc/haramnavigation/MainActivity.kt
@@ -2,10 +2,24 @@ package com.inhatc.haramnavigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.inhatc.haramnavigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // 데이터 바인딩 사용
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.apply {
+            // NavController 획득
+            val navController = findNavController(R.id.demo_nav_host_fragment)
+            bnvMain.setupWithNavController(navController)
+        }
     }
 }
